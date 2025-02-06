80ced8c34ab7cb323704498cf7b3fd42159cf5a2
Pastamaster74 <aku.playstation@gmail.com>
2025-02-06 17:07:45 +0200

    Update Main.java
    Added settings

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-package todoListApp;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.*;
@@ -33,11 +32,47 @@ public class Main {
 		addButton.setBackground(new Color(153, 148, 148));
 		addButton.setForeground(Color.WHITE);
 		
+		JButton settingsButton = new JButton("Settings");
+		settingsButton.setBounds(10, 435, 100, 20);
+		settingsButton.setFont(new Font("sans-serif", Font.PLAIN, 14));
+		settingsButton.setBackground(new Color(153, 148, 148));
+		settingsButton.setForeground(Color.WHITE);
+		
+		JFrame settingsFrame = new JFrame("Settings");
+		settingsFrame.setBounds(mainFrame.getX() + 35, mainFrame.getY() + 35, 380, 480);
+		settingsFrame.setResizable(false);
+		settingsFrame.setLayout(null);
+		settingsFrame.getContentPane().setBackground(new Color(122, 118, 118));
+		settingsFrame.setVisible(false);
+		
+		JLabel headerText2 = new JLabel("-- Settings --");
+		headerText2.setBounds(100, 0, 300, 100);
+		headerText2.setFont(new Font("sans-serif", Font.ITALIC, 30));
+		headerText2.setForeground(Color.WHITE);
+		
+		JButton darkThemeButton = new JButton("Dark theme");
+		darkThemeButton.setBounds(40, 90, 125, 30);
+		darkThemeButton.setFont(new Font("sans-serif", Font.PLAIN, 14));
+		darkThemeButton.setBackground(new Color(153, 148, 148));
+		darkThemeButton.setForeground(Color.WHITE);
+		
+		JButton lightThemeButton = new JButton("Light theme");
+		lightThemeButton.setBounds(215, 90, 125, 30);
+		lightThemeButton.setFont(new Font("sans-serif", Font.PLAIN, 14));
+		lightThemeButton.setBackground(new Color(153, 148, 148));
+		lightThemeButton.setForeground(Color.WHITE);
+		
 		mainFrame.add(headerText);
 		mainFrame.add(textField);
 		mainFrame.add(addButton);
+		mainFrame.add(settingsButton);
+		
+		settingsFrame.add(headerText2);
+		settingsFrame.add(darkThemeButton);
+		settingsFrame.add(lightThemeButton);
 		
 		mainFrame.repaint();
+		settingsFrame.repaint();
 		
 		addButton.addActionListener(new ActionListener() {
 
@@ -72,6 +107,66 @@ public class Main {
 			
 		});
 
-	}
+		settingsButton.addActionListener(new ActionListener() {
 
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				settingsFrame.setBounds(mainFrame.getX() + 35, mainFrame.getY() + 35, 380, 480);
+				settingsFrame.setVisible(true);
+			}
+			
+		});
+		
+		darkThemeButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mainFrame.getContentPane().setBackground(new Color(122, 118, 118));
+				headerText.setForeground(Color.WHITE);
+				textField.setBackground(new Color(153, 148, 148));
+				textField.setForeground(Color.WHITE);
+				addButton.setBackground(new Color(153, 148, 148));
+				addButton.setForeground(Color.WHITE);
+				settingsButton.setBackground(new Color(153, 148, 148));
+				settingsButton.setForeground(Color.WHITE);
+				settingsFrame.getContentPane().setBackground(new Color(122, 118, 118));
+				headerText2.setForeground(Color.WHITE);
+				darkThemeButton.setBackground(new Color(153, 148, 148));
+				darkThemeButton.setForeground(Color.WHITE);
+				lightThemeButton.setBackground(new Color(153, 148, 148));
+				lightThemeButton.setForeground(Color.WHITE);
+				
+				mainFrame.repaint();
+				settingsFrame.repaint();
+			}
+			
+		});
+		
+		lightThemeButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mainFrame.getContentPane().setBackground(new Color(237, 242, 239));
+				headerText.setForeground(Color.BLACK);
+				textField.setBackground(new Color(213, 219, 215));
+				textField.setForeground(Color.BLACK);
+				addButton.setBackground(new Color(213, 219, 215));
+				addButton.setForeground(Color.BLACK);
+				settingsButton.setBackground(new Color(213, 219, 215));
+				settingsButton.setForeground(Color.BLACK);
+				settingsFrame.getContentPane().setBackground(new Color(237, 242, 239));
+				headerText2.setForeground(Color.BLACK);
+				darkThemeButton.setBackground(new Color(213, 219, 215));
+				darkThemeButton.setForeground(Color.BLACK);
+				lightThemeButton.setBackground(new Color(213, 219, 215));
+				lightThemeButton.setForeground(Color.BLACK);
+				
+				mainFrame.repaint();
+				settingsFrame.repaint();
+			}
+			
+		});
+		
+	}
+	
 }
